2a5f4d3a768ed4585a417dd8d37758ff83242efa
FosterClark48 <fosterclark48@yahoo.com>
2024-01-03 17:20:45 -0600

    created new elements to append to body and added a function that updates click count and modifies text content with a lodash debouncer attached to click event to prevent spammy behavior

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import { debounce } from 'lodash';
+
+// Create new elements and append to body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+// Function to update count and modify text content
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+// Select button and bind click event w/ debounced updateCounter to prevent spammy behavior
+$('button').on('click', debounce(updateCounter, 250));
