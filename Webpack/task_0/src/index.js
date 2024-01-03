a44cf8725e1032cad0c65f968ce3281ae5866f22
FosterClark48 <fosterclark48@yahoo.com>
2024-01-03 16:25:30 -0600

    imported jquery and created new paragraphs to dynamically append to the body of the index.html as a bundle through main.js

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+// Create new paragraphs and append to the body
+
+$('body').append('<p>Holberton Dashboard</p>');
+
+$('body').append('<p>Dashboard data for the students</p>');
+
+$('body').append('<p>Copyright - Holberton School</p>');
