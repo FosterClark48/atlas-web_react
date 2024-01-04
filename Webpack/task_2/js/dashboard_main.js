243a0052bcba61a6c1add1883c1590576ff6494f
FosterClark48 <fosterclark48@yahoo.com>
2024-01-04 11:01:55 -0600

    Added jquery for logo div so logo shows up, and added import for the css file for styling

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+// Create new elements and append to body
+$('body').append('<div id="logo"><div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+// Function to update count and modify text content
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+// Select button and bind click event w/ debounced updateCounter to prevent spammy behavior
+$('button').on('click', _.debounce(updateCounter, 500));
