fca270f9a9522800a2548239dede54f0d5346f28
FosterClark48 <fosterclark48@yahoo.com>
2024-01-04 15:35:49 -0600

    divided up main.js and main.css files into modules folder with 3 different entry points

---
+++ Webpack/task_3/modules/body/body.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css';
+
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+
+let count = 0;
+
+// Function to update count and modify text content
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+// Select button and bind click event w/ debounced updateCounter to prevent spammy behavior
+$('button').on('click', _.debounce(updateCounter, 500));
