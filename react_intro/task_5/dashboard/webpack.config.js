6822ddcf20e9b6df3df6572bdd2359049a96754b
FosterClark48 <fosterclark48@yahoo.com>
2024-01-07 18:26:49 -0600

    created config file for setting up a new project

---
+++ react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+
+  // Output bundle.js to dist folder
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    hot: true,
+  },
+
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // regex to tell Webpack what files to transform
+        use: [
+          'style-loader', // injects CSS to the DOM by injecting a <style> tag
+          'css-loader', // interprets @import and url() like import/require() & will resolve them
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
