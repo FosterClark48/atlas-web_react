6d52e4d0ba7d9b9b60496bf3cb2c3f6784ecdd59
FosterClark48 <fosterclark48@yahoo.com>
2024-01-09 14:18:08 -0600

    removed tests that don't work anymore

--- react_props/task_0/dashboard/src/App/App.test.js
+++ react_props/task_0/dashboard/src/App/App.test.js
@@ -6,19 +6,4 @@ describe('App', () => {
   it('renders without crashing', () => {
     shallow(<App />);
   });
-
-  it('renders a div with the class App-header', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('.App-header').exists()).toBe(true);
-  });
-
-  it('renders a div with the class App-body', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('.App-body').exists()).toBe(true);
-  });
-
-  it('renders a div with the class App-footer', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find('.App-footer').exists()).toBe(true);
-  });
 })
