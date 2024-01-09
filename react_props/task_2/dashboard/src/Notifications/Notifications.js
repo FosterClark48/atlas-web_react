9623d35ddc4234bbc0ce70a9a45825fd02804f33
FosterClark48 <fosterclark48@yahoo.com>
2024-01-09 15:19:13 -0600

    copied from last task

---
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,49 @@
+import React from "react";
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import { getLatestNotification } from "../utils/utils";
+
+export function Notifications() {
+
+  // inline styling for button
+  const buttonStyle = {
+    border: 'none',
+    background: 'transparent',
+    cursor: 'pointer',
+    padding: '0'
+  };
+
+  // inline styling for icon
+  const iconStyle = {
+    width: '1rem',
+    height: '1rem',
+    margin: '0.5rem'
+  };
+
+  const handleButtonClick = () => {
+    console.log("close button has been clicked");
+  };
+
+  return (
+    <div className="Notifications">
+      <div className="Notifications-content">
+        <p>
+          Here is the list of notifications
+        </p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li className="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        </ul>
+      </div>
+      <button
+        aria-label="Close"
+        style={buttonStyle}
+        onClick={handleButtonClick}>
+          <img src={closeIcon} alt="Close" style={iconStyle} />
+      </button>
+    </div>
+  );
+}
+
+export default Notifications;
