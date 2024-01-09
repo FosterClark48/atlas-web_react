d3fbcbc84ffe89caf1fc487ddcdfc1b98028b20c
FosterClark48 <fosterclark48@yahoo.com>
2024-01-09 17:47:48 -0600

    updated and added tests to test the new functional component NotificationItem

--- react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
 
 describe('Notifications', () => {
   it('renders without crashing', () => {
     shallow(<Notifications />);
   });
 
-  it('renders three list items', () => {
+  it('renders NotificationItem elements', () => {
     const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('li').length).toBe(3);
+    expect(wrapper.find(NotificationItem).length).toBe(3);
   });
 
   it('renders the text Here is the list of notifications', () => {
     const wrapper = shallow(<Notifications />);
     expect(wrapper.contains('Here is the list of notifications')).toEqual(true);
+  });
+
+  it('first NotificationItem element renders the right html', () => {
+    const wrapper = shallow(<Notifications />);
+    const firstNotificationItem = wrapper.find(NotificationItem).first();
+    expect(firstNotificationItem.prop('type')).toEqual('default');
+    expect(firstNotificationItem.shallow().text()).toContain('New course available');
   });
 })
