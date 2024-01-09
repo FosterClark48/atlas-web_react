ccf184f88b017cde93eb2c83dd5786cc3a6f0a6c
FosterClark48 <fosterclark48@yahoo.com>
2024-01-09 14:08:00 -0600

    Added components to put in App.js to use as the shell

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,35 +1,20 @@
 import React from "react";
-import logo from '../assets/atlas_logo.png';
 import './App.css';
-import { getFullYear, getFooterCopy } from '../utils/utils';
+import Notifications from "../Notifications/Notifications";
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          School dashboard
-        </h1>
-      </header>
-      <div className="App-body">
-        <p>
-          Login to access the full dashboard
-        </p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="text" id="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password"></input>
-          <button type="submit">OK</button>
-        </form>
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
       </div>
-      <footer className="App-footer">
-        <p>
-          Copyright {getFullYear()} - {getFooterCopy(true)}
-        </p>
-      </footer>
-    </div>
+    </>
   );
 }
 
