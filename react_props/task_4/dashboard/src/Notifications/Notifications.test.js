82a5e0e27985c63a284fc2a58e808392cbaf2bd6
FosterClark48 <fosterclark48@yahoo.com>
2024-01-10 01:54:54 -0600

    Added tests for Notifications file

--- react_props/task_4/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -8,20 +8,40 @@ describe('Notifications', () => {
     shallow(<Notifications />);
   });
 
-  it('renders NotificationItem elements', () => {
-    const wrapper = shallow(<Notifications />);
+  it('renders NotificationItem elements when displayDrawer is true', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
     expect(wrapper.find(NotificationItem).length).toBe(3);
   });
 
-  it('renders the text Here is the list of notifications', () => {
-    const wrapper = shallow(<Notifications />);
+  it('renders the text "Here is the list of notifications" when displayDrawer is true', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
     expect(wrapper.contains('Here is the list of notifications')).toEqual(true);
   });
 
-  it('first NotificationItem element renders the right html', () => {
-    const wrapper = shallow(<Notifications />);
+  it('first NotificationItem element renders the right html when displayDrawer is true', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
     const firstNotificationItem = wrapper.find(NotificationItem).first();
     expect(firstNotificationItem.prop('type')).toEqual('default');
     expect(firstNotificationItem.shallow().text()).toContain('New course available');
+  });
+
+  it('displays the menu item when displayDrawer is false', () => {
+    const wrapper = shallow(<Notifications displayDrawer={false} />);
+    expect(wrapper.find('.menuItem').length).toBe(1);
+  });
+
+  it('does not display div.Notifications when displayDrawer is false', () => {
+    const wrapper = shallow(<Notifications displayDrawer={false} />);
+    expect(wrapper.find('.Notifications').length).toBe(0);
+  });
+
+  it('displays the menu item when displayDrawer is true', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+    expect(wrapper.find('.menuItem').length).toBe(1);
+  });
+
+  it('displays div.Notifications when displayDrawer is true', () => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+    expect(wrapper.find('.Notifications').length).toBe(1);
   });
 })
