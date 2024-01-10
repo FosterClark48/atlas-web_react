28e93f8f77897bd40e326071bb15e6cabcb0a95c
FosterClark48 <fosterclark48@yahoo.com>
2024-01-10 12:16:03 -0600

    Added arrays for listCourses and listNotifications and implemented them in JSX

--- react_props/task_5/dashboard/src/App/App.js
+++ react_props/task_5/dashboard/src/App/App.js
@@ -8,16 +8,30 @@ import CourseList from "../CourseList/CourseList";
 import PropTypes from 'prop-types';
 
 function App({ isLoggedIn }) {
+
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+  ]
+
+
   return (
     <>
       <div className="App-header-wrapper">
         <Header />
         <div className="App-menu-notifications">
-          <Notifications />
+          <Notifications listNotifications={listNotifications} />
         </div>
       </div>
       <div className="App">
-        {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
         <Footer />
       </div>
     </>
