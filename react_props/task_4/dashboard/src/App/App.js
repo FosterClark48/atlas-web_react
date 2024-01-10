35cfce36b29bb76ba6e67459f4a7b28b20498fab
FosterClark48 <fosterclark48@yahoo.com>
2024-01-10 01:53:58 -0600

    Added prop types, added containers for the header and notification components for styling. added conditional rendering for CourseList & Login

--- react_props/task_4/dashboard/src/App/App.js
+++ react_props/task_4/dashboard/src/App/App.js
@@ -4,18 +4,32 @@ import Notifications from "../Notifications/Notifications";
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import CourseList from "../CourseList/CourseList";
+import PropTypes from 'prop-types';
 
-function App() {
+function App({ isLoggedIn }) {
   return (
     <>
-      <Notifications />
-      <div className="App">
+      <div className="App-header-wrapper">
         <Header />
-        <Login />
+        <div className="App-menu-notifications">
+          <Notifications />
+        </div>
+      </div>
+      <div className="App">
+        {isLoggedIn ? <CourseList /> : <Login />}
         <Footer />
       </div>
     </>
   );
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
 
 export default App;
