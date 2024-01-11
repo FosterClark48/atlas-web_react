f97156c07641a7bc0799566fce4125d2ea48e5e8
FosterClark48 <fosterclark48@yahoo.com>
2024-01-11 00:05:22 -0600

    added TextEncoder globally

--- React_component/task_1/dashboard/config/setupTests.js
+++ React_component/task_1/dashboard/config/setupTests.js
@@ -1,4 +1,7 @@
+if (typeof TextEncoder === "undefined") {
+  global.TextEncoder = require("util").TextEncoder;
+}
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
