50ce3340ad327815aec9e7adc175074f6140d723
FosterClark48 <fosterclark48@yahoo.com>
2024-01-11 00:21:07 -0600

    changed functional component to class component and included markAsRead method and passed it to each NotificationItem

--- React_component/task_2/dashboard/src/Notifications/Notifications.js
+++ React_component/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import './Notifications.css';
 import NotificationItem from "./NotificationItem";
 import { NotificationItemShape } from "./NotificationItemShape";
@@ -6,65 +6,69 @@ import closeIcon from '../assets/close-icon.png';
 // import { getLatestNotification } from "../utils/utils";
 import PropTypes from 'prop-types';
 
-export function Notifications({ displayDrawer, listNotifications }) {
+class Notifications extends Component {
 
-  // inline styling for button
-  const buttonStyle = {
-    border: 'none',
-    background: 'transparent',
-    cursor: 'pointer',
-    padding: '0'
-  };
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
 
-  // inline styling for icon
-  const iconStyle = {
-    width: '.8rem',
-    height: '.8rem',
-    margin: '0.5rem'
-  };
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+    const buttonStyle = {
+      border: 'none',
+      background: 'transparent',
+      cursor: 'pointer',
+      padding: '0'
+    };
+    const iconStyle = {
+      width: '.8rem',
+      height: '.8rem',
+      margin: '0.5rem'
+    };
+    const handleButtonClick = () => {
+      console.log("close button has been clicked");
+    };
 
-  const handleButtonClick = () => {
-    console.log("close button has been clicked");
-  };
-
-  return (
-    <>
-      <div className="menuItem">
-        <p>Your Notifications</p>
-      </div>
-      {displayDrawer && (
-        <div className="Notifications">
-          <div className="Notifications-content">
-            {listNotifications.length > 0 && (
-              <p>
-                Here is the list of notifications
-              </p>
-            )}
-            <ul>
-              {listNotifications.length === 0 ? (
-                <NotificationItem value='No new notification for now' />
-              ) : (
-                listNotifications.map(notification => (
-                  <NotificationItem
-                    key={notification.id}
-                    type={notification.type}
-                    value={notification.value}
-                    html={notification.html}
-                  />
-                ))
-              )}
-            </ul>
-          </div>
-          <button
-            aria-label="Close"
-            style={buttonStyle}
-            onClick={handleButtonClick}>
-              <img src={closeIcon} alt="Close" style={iconStyle} />
-          </button>
+    return (
+      <>
+        <div className="menuItem">
+          <p>Your Notifications</p>
         </div>
-      )}
-    </>
-  );
+        {displayDrawer && (
+          <div className="Notifications">
+            <div className="Notifications-content">
+              {listNotifications.length > 0 && (
+                <p>
+                  Here is the list of notifications
+                </p>
+              )}
+              <ul>
+                {listNotifications.length === 0 ? (
+                  <NotificationItem value='No new notification for now' />
+                ) : (
+                  listNotifications.map(notification => (
+                    <NotificationItem
+                      key={notification.id}
+                      type={notification.type}
+                      value={notification.value}
+                      html={notification.html}
+                      markAsRead={() => this.markAsRead(notification.id)}
+                    />
+                  ))
+                )}
+              </ul>
+            </div>
+            <button
+              aria-label="Close"
+              style={buttonStyle}
+              onClick={handleButtonClick}>
+                <img src={closeIcon} alt="Close" style={iconStyle} />
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
 }
 
 Notifications.propTypes = {
