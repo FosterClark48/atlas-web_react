b485ffae96882dff533d08c9706818d8395a8d2b
FosterClark48 <fosterclark48@yahoo.com>
2024-01-10 18:13:14 -0600

    transformed the functional component to a class component

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 import Notifications from "../Notifications/Notifications";
 import Header from '../Header/Header';
@@ -7,35 +7,42 @@ import Login from '../Login/Login';
 import CourseList from "../CourseList/CourseList";
 import PropTypes from 'prop-types';
 
-function App({ isLoggedIn }) {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+      ],
+    };
+  }
 
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+  render () {
+    const { isLoggedIn } = this.props;
+    const { listCourses, listNotifications } = this.state;
 
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
-  ]
-
-
-  return (
-    <>
-      <div className="App-header-wrapper">
-        <Header />
-        <div className="App-menu-notifications">
-          <Notifications listNotifications={listNotifications} />
+    return (
+      <>
+        <div className="App-header-wrapper">
+          <Header />
+          <div className="App-menu-notifications">
+            <Notifications listNotifications={listNotifications} />
+          </div>
         </div>
-      </div>
-      <div className="App">
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        <Footer />
-      </div>
-    </>
-  );
+        <div className="App">
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 App.propTypes = {
