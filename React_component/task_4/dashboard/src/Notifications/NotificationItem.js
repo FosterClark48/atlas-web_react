e661fc6a6beb0cb842d065c9e077ac7c02373fb8
FosterClark48 <fosterclark48@yahoo.com>
2024-01-15 00:37:24 -0600

    changed regular component to PureComponent

--- React_component/task_4/dashboard/src/Notifications/NotificationItem.js
+++ React_component/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 
-class NotificationItem extends Component {
+class NotificationItem extends PureComponent {
 
   render() {
     const { id, type, html, value, markAsRead } = this.props;
