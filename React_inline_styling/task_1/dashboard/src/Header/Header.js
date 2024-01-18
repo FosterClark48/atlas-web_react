0e1b4cfa4610dbfa87024795ece82eed635db11d
FosterClark48 <fosterclark48@yahoo.com>
2024-01-17 18:14:26 -0600

    removed CSS styling and added aphrodite inline styling

--- React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import logo from '../assets/atlas_logo.png';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+
+  logo: {
+    height: '12rem',
+    pointerEvents: 'none',
+  },
+
+  headerMain: {
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  headerTitle: {
+    fontFamily: "'Galano Grotesque Alt', sans-serif",
+    fontSize: '3.5rem',
+    color: '#00003C',
+    fontWeight: '800',
+  },
+});
 
 
 function Header() {
   return (
-    <header className="Header-main">
-        <img src={logo} className="Header-logo" alt="logo" />
-        <h1>
+    <header className={css(styles.headerMain)}>
+        <img src={logo} className={css(styles.logo)} alt="logo" />
+        <h1 className={css(styles.headerTitle)}>
           School dashboard
         </h1>
     </header>
