77cfc46c7cf5d4dcf2efb957b6c0acd5fcb46495
FosterClark48 <fosterclark48@yahoo.com>
2024-01-18 10:47:14 -0600

    Removed CSS styling and changed it to aphrodite inline styling. applied conditional rendering to data-notification-type to choose the styling based on whether its urgent or default priority

--- React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
+++ React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,16 +1,42 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+
+  listDefault: {
+    fontFamily: "'Galano Grotesque Alt', sans-serif",
+    fontWeight: '400',
+    fontSize: '0.8rem',
+    color: 'blue',
+  },
+
+  listUrgent: {
+    fontFamily: "'Galano Grotesque Alt', sans-serif",
+    fontWeight: '400',
+    fontSize: '0.8rem',
+    color: 'red',
+  },
+
+  listHtml: {
+    fontFamily: "'Galano Grotesque Alt', sans-serif",
+    fontWeight: '400',
+    fontSize: '0.9rem',
+    color: 'red',
+  },
+});
 
 
 class NotificationItem extends PureComponent {
 
   render() {
     const { id, type, html, value, markAsRead } = this.props;
+    const priorityType = type === 'urgent' ? styles.listUrgent : styles.listDefault;
 
     return html ? (
-      <li data-notification-type={type} dangerouslySetInnerHTML={html} onClick={() => markAsRead(id)}></li>
+      <li className={css(styles.listHtml)} data-notification-type={type} dangerouslySetInnerHTML={html} onClick={() => markAsRead(id)}></li>
     ) : (
-      <li data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li>
+      <li className={css(priorityType)} data-notification-type={type} onClick={() => markAsRead(id)}>{value}</li>
     );
   }
 }
