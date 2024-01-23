67a7c6808d238f74821c8fd600dfb276f7dfdcda
FosterClark48 <fosterclark48@yahoo.com>
2024-01-23 01:25:19 -0600

    Added tests that check the new states changes

--- react_state/task_1/dashboard/src/Login/Login.test.js
+++ react_state/task_1/dashboard/src/Login/Login.test.js
@@ -16,13 +16,30 @@ describe('Login', () => {
     shallow(<Login />);
   });
 
-  it('renders 2 input tags', () => {
+  it('renders 3 input tags', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find('input').length).toEqual(2);
+    expect(wrapper.find('input').length).toEqual(3);
   });
 
   it('renders 2 label tags', () => {
     const wrapper = shallow(<Login />);
     expect(wrapper.find('label').length).toEqual(2);
+  });
+
+  it('submit button is disabled by default', () => {
+    const wrapper = shallow(<Login />);
+    const submitButton = wrapper.find('input[type="submit"]');
+    expect(submitButton.props().disabled).toBe(true);
+  });
+
+  it('submit button is enabled when both email and password are provided', () => {
+    const wrapper = shallow(<Login />);
+    const emailInput = wrapper.find('input[type="email"]');
+    const passwordInput = wrapper.find('input[type="password"]');
+    emailInput.simulate('change', { target: { value: 'user@example.com' } });
+    passwordInput.simulate('change', { target: { value: 'password' } });
+
+    const submitButton = wrapper.find('input[type="submit"]');
+    expect(submitButton.props().disabled).toBe(false);
   });
 })
