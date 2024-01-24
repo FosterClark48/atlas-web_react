3ff12ac5a6ba230686c2d813e1e81b0e098f63d4
FosterClark48 <fosterclark48@yahoo.com>
2024-01-23 19:01:10 -0600

    Added a context consumer to the footer component to subscribe to the UserContext

--- react_state/task_3/dashboard/src/Footer/Footer.js
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,13 +1,25 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { getFullYear, getFooterCopy } from '../utils/utils';
+import UserContext from '../App/AppContext';
 
 function Footer({ footerClassName }) {
   return (
     <footer className={footerClassName}>
-        <p>
-          Copyright {getFullYear()} - {getFooterCopy(true)}
-        </p>
+      <UserContext.Consumer>
+        {({ user }) => (
+          <>
+            <p>
+              Copyright {getFullYear()} - {getFooterCopy(true)}
+            </p>
+            {user.isLoggedIn && (
+              <p>
+                <a href="mailto:contact@school.com"> Contact us</a>
+              </p>
+            )}
+          </>
+        )}
+      </UserContext.Consumer>
     </footer>
   );
 }
