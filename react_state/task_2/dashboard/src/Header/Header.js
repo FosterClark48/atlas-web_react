56d111bc7a623da0de9268b46f79fbf75c10289c
FosterClark48 <fosterclark48@yahoo.com>
2024-01-23 18:39:37 -0600

    updated header component to use user and logout states instead of props, added logout functionality and welcome message

--- react_state/task_2/dashboard/src/Header/Header.js
+++ react_state/task_2/dashboard/src/Header/Header.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/atlas_logo.png';
 import { StyleSheet, css } from 'aphrodite';
+import UserContext from '../App/AppContext';
 
 const styles = StyleSheet.create({
 
@@ -32,18 +33,53 @@ const styles = StyleSheet.create({
       maxWidth: '100%', // Ensures text does not overflow the header width
     },
   },
+
+  logoutSection: {
+    fontFamily: "'Galano Grotesque Alt', sans-serif",
+    fontWeight: '400',
+    fontSize: '1.3rem',
+    margin: '1rem 2rem 1rem 4rem',
+  },
+
+  logoutButton: {
+    backgroundColor: '#1ED2AF',
+    border: '1px solid #00003C',
+    borderRadius: '8px',
+    padding: '.4rem',
+    boxShadow: 'rgba(213, 217, 217, .5) 0 2px 5px 0',
+    cursor: 'pointer',
+    textAlign: 'center',
+    textDecoration: 'none',
+    ':focus': {
+      borderColor: '#1ED2AF',
+      outline: '2px solid #1ED2AF',
+    },
+  }
 });
 
 
-function Header() {
-  return (
-    <header className={css(styles.headerMain)}>
-        <img src={logo} className={css(styles.logo)} alt="logo" />
-        <h1 className={css(styles.headerTitle)}>
-          School dashboard
-        </h1>
-    </header>
-  );
+class Header extends Component {
+  static contextType = UserContext;
+
+  render () {
+    const { user, logOut } = this.context;
+
+    return (
+      <header className={css(styles.headerMain)}>
+          <img src={logo} className={css(styles.logo)} alt="logo" />
+          <h1 className={css(styles.headerTitle)}>
+            School dashboard
+          </h1>
+          {user.isLoggedIn && (
+            <div id="logoutSection" className={css(styles.logoutSection)}>
+              <p>
+                Welcome {user.email} <button className={css(styles.logoutButton)} onClick={logOut}>logout</button>
+              </p>
+            </div>
+          )}
+      </header>
+    );
+  }
 }
 
 export default Header;
