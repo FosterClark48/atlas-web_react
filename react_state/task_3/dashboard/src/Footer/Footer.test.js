b14b9420250ed7063edceb753e2fe6fbc15f6d3a
FosterClark48 <fosterclark48@yahoo.com>
2024-01-23 20:22:19 -0600

    updated tests to work with the Context Consumer

--- react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Footer from './Footer';
 import { StyleSheetTestUtils } from 'aphrodite';
+import UserContext from '../App/AppContext';
 
 describe('Footer', () => {
   beforeAll(() => {
@@ -11,13 +12,47 @@ describe('Footer', () => {
   afterAll(() => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
-  
+
+  const userLoggedIn = {
+    user: { isLoggedIn: true },
+  };
+
+  const userLoggedOut = {
+    user: { isLoggedIn: false },
+  };
+
   it('renders without crashing', () => {
-    shallow(<Footer />);
+    shallow(
+      <UserContext.Provider value={userLoggedOut}>
+        <Footer />
+      </UserContext.Provider>
+    );
   });
 
   it('renders the text Copyright', () => {
-    const wrapper = shallow(<Footer />);
+    const wrapper = mount(
+      <UserContext.Provider value={userLoggedOut}>
+        <Footer />
+      </UserContext.Provider>
+    );
     expect(wrapper.text().includes('Copyright')).toEqual(true);
+  });
+
+  it ('does not display the Contact us link when the user is logged out', () => {
+    const wrapper = mount(
+      <UserContext.Provider value={userLoggedOut}>
+        <Footer />
+      </UserContext.Provider>
+    );
+    expect(wrapper.text().includes('Contact us')).toEqual(false);
+  });
+
+  it('displays the Contact us link when the user is logged in', () => {
+    const wrapper = mount(
+      <UserContext.Provider value={userLoggedIn}>
+        <Footer />
+      </UserContext.Provider>
+    );
+    expect(wrapper.text().includes('Contact us')).toEqual(true);
   });
 })
