00845eba703e86b590f7b7c80765d8bd887dc6bc
FosterClark48 <fosterclark48@yahoo.com>
2024-01-28 23:36:30 -0600

    copied from last task

---
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,22 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+import { useDispatch } from 'react-redux';
+
+export const selectCourse = index => {
+  return {
+    type: SELECT_COURSE,
+    index
+  }
+}
+
+export const unselectCourse = index => {
+  return {
+    type: UNSELECT_COURSE,
+    index
+  }
+}
+
+// Higher-order function to bind action creators
+export const bindCourseActionCreators = (dispatch) => ({
+  boundSelectCourse: (index) => dispatch(selectCourse(index)),
+  boundUnselectCourse: (index) => dispatch(unselectCourse(index))
+});
