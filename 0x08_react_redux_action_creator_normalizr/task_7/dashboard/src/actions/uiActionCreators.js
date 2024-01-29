f9642176d74797836c319141d1c25c942df169df
FosterClark48 <fosterclark48@yahoo.com>
2024-01-29 00:58:31 -0600

    added a couple action creators and a loginRequest function

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,4 +1,4 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from "./uiActionTypes";
 
 export const login = (email, password) => {
   return {
@@ -24,6 +24,39 @@ export const hideNotificationDrawer = () => {
     type: HIDE_NOTIFICATION_DRAWER,
   }
 }
+
+export const loginSuccess = () => {
+  return {
+    type: LOGIN_SUCCESS,
+  }
+}
+
+export const loginFailure = () => {
+  return {
+    type: LOGIN_FAILURE,
+  }
+}
+
+export const loginRequest = (email, password) => {
+  return async (dispatch) => {
+    try {
+      // Dispatch the login action
+      dispatch(login(email, password));
+
+      //Perform API call
+      const response = await fetch('/login-success.json');
+      if(!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      //Dispatch loginSuccess when fetch is successful
+      dispatch(loginSuccess());
+    } catch (error) {
+      // Dispatch loginFailure when the fetch fails
+      dispatch(loginFailure());
+    }
+  };
+};
 
 // Higher-order function to bind UI action creators
 export const bindUIActionCreators = (dispatch) => ({
