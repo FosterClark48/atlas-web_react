4e7f7886357b3ff9beb40dca2824d08b62238ccc
FosterClark48 <fosterclark48@yahoo.com>
2024-02-08 11:49:58 -0600

    copied from last task

---
+++ react_redux_connectors_and_providers/task_2/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,59 @@
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from "./uiActionTypes";
+
+export const login = (email, password) => {
+  return {
+    type: LOGIN,
+    user: { email, password },
+  }
+}
+
+export const logout = () => {
+  return {
+    type: LOGOUT,
+  }
+}
+
+export const displayNotificationDrawer = () => {
+  return {
+    type: DISPLAY_NOTIFICATION_DRAWER,
+  }
+}
+
+export const hideNotificationDrawer = () => {
+  return {
+    type: HIDE_NOTIFICATION_DRAWER,
+  }
+}
+
+export const loginSuccess = () => {
+  return {
+    type: LOGIN_SUCCESS,
+  }
+}
+
+export const loginFailure = () => {
+  return {
+    type: LOGIN_FAILURE,
+  }
+}
+
+export const loginRequest = (email, password) => {
+  return async (dispatch) => {
+    try {
+      // Dispatch the login action
+      dispatch(login(email, password));
+
+      //Perform API call
+      const response = await fetch('/login-success.json');
+      if(!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      //Dispatch loginSuccess when fetch is successful
+      dispatch(loginSuccess());
+    } catch (error) {
+      // Dispatch loginFailure when the fetch fails
+      dispatch(loginFailure());
+    }
+  };
+};
