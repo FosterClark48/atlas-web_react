9b074b06c0efc9f945fcdb9b5cf41f9a053c2d17
FosterClark48 <fosterclark48@yahoo.com>
2024-02-10 16:44:37 -0600

    connected Footer component to Redux store to access the user's login state and conditionally renders based on this state

--- react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,25 +1,22 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { getFullYear, getFooterCopy } from '../utils/utils';
-import UserContext from '../App/AppContext';
+import { connect } from 'react-redux';
+import { Map } from 'immutable';
 
-function Footer({ footerClassName }) {
+function Footer({ footerClassName, user }) {
   return (
     <footer className={footerClassName}>
-      <UserContext.Consumer>
-        {({ user }) => (
-          <>
-            <p>
-              Copyright {getFullYear()} - {getFooterCopy(true)}
-            </p>
-            {user.isLoggedIn && (
-              <p>
-                <a href="mailto:contact@school.com"> Contact us</a>
-              </p>
-            )}
-          </>
+      <>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+        {user.isLoggedIn && (
+          <p>
+            <a href="mailto:contact@school.com"> Contact us</a>
+          </p>
         )}
-      </UserContext.Consumer>
+      </>
     </footer>
   );
 }
@@ -28,4 +25,10 @@ Footer.propTypes = {
   footerClassName: PropTypes.string,
 };
 
-export default Footer;
+export function mapStateToProps(state) {
+  const uiReducer = state.get('uiReducer', Map());
+  const isLoggedIn = uiReducer.get('isUserLoggedIn', false);
+  return { user: { isLoggedIn } };
+};
+
+export default connect(mapStateToProps)(Footer);
