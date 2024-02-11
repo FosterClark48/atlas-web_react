7ee79a2e6d1604a551d2efcb0148c7c4e84844fa
FosterClark48 <fosterclark48@yahoo.com>
2024-02-11 00:20:01 -0600

    updated the reducer

--- react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -1,10 +1,11 @@
-import { MARK_AS_READ, SET_TYPE_FILTER, FETCH_NOTIFICATIONS_SUCCESS, NotificationTypeFilters } from "../actions/notificationActionTypes";
+import { MARK_AS_READ, SET_TYPE_FILTER, FETCH_NOTIFICATIONS_SUCCESS, SET_LOADING_STATE, NotificationTypeFilters } from "../actions/notificationActionTypes";
 import { notificationsNormalizer } from "../schema/notifications";
 import { fromJS } from 'immutable';
 
 const initialState = fromJS({
   notifications: {},
   filter: NotificationTypeFilters.DEFAULT,
+  loading: false,
 });
 
 function notificationReducer(state = initialState, action) {
@@ -16,6 +17,8 @@ function notificationReducer(state = initialState, action) {
       return state.setIn(['notifications', action.index, 'isRead'], true);
     case SET_TYPE_FILTER:
       return state.set('filter', action.filter);
+    case SET_LOADING_STATE:
+      return state.set('loading', action.isLoading);
     default:
       return state;
   }
